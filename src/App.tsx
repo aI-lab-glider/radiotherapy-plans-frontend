993452b3641e603299f7c581320ba25adf5a1245
Marcin Wachacki <mawachacki@gmail.com>
2021-11-03 20:37:28 +0100

    Tabs with mock chart were added

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,15 @@ import Divider from "@material-ui/core/Divider";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
 import ThreeCanvas from "./components/ThreeCanvas";
+import DVH from "./components/DVH";
 import Upload from "./components/Upload";
 import DrawerToolbar from "./components/DrawerToolbar";
 import { UploadState } from "./reducers/uploadReducer";
+import { TabPanel, a11yProps } from "./components/TabPanel";
 
 const drawerWidth = 360;
 const appBarHeight = 60;
@@ -75,6 +80,12 @@ const useStyles = makeStyles((theme: Theme) =>
 export default function App() {
   const classes = useStyles();
   const [open, setOpen] = React.useState(true);
+  const [tabIndex, setTabIndex] = React.useState(0);
+  const TABS = {
+    VISUALIZATION: 0,
+    DVH: 1,
+  };
+
   const isFileUploaded = useSelector(
     (state: UploadState) => state.isFileUploaded
   );
@@ -85,6 +96,10 @@ export default function App() {
 
   const handleDrawerClose = () => {
     setOpen(false);
+  };
+
+  const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
+    setTabIndex(newValue);
   };
 
   return (
@@ -126,11 +141,36 @@ export default function App() {
           </IconButton>
         </div>
         <Divider />
-        {isFileUploaded ? <DrawerToolbar /> : <Upload />}
+        {isFileUploaded ? (
+          <Box>
+            <Box>
+              <Tabs
+                value={tabIndex}
+                onChange={handleTabChange}
+                aria-label="basic tabs example"
+                centered
+              >
+                <Tab label="Visualization" {...a11yProps(TABS.VISUALIZATION)} />
+                <Tab label="DVH" {...a11yProps(TABS.DVH)} />
+              </Tabs>
+            </Box>
+            <TabPanel value={tabIndex} index={TABS.VISUALIZATION}>
+              <DrawerToolbar />
+            </TabPanel>
+            <TabPanel value={tabIndex} index={TABS.DVH}>
+              DVH of the structures
+            </TabPanel>
+          </Box>
+        ) : (
+          <Upload />
+        )}
       </Drawer>
 
       <div className={classes.header} />
-      <ThreeCanvas appBarHeight={appBarHeight} />
+      {tabIndex === TABS.VISUALIZATION && (
+        <ThreeCanvas appBarHeight={appBarHeight} />
+      )}
+      {tabIndex === TABS.DVH && <DVH />}
     </div>
   );
 }
